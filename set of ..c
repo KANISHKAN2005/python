33a7b089aec166c847248b57aa63cb2fc91889e2
KANISHKAN2005 <115133801+KANISHKAN2005@users.noreply.github.com>
2023-04-25 11:31:41 +0530

    Add files via upload

---
+++ set of ..c
@@ -0,0 +1,147 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 12,
+   "id": "9247eacb",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enternum1:6\n"
+     ]
+    }
+   ],
+   "source": [
+    "A=input(\"enternum1:\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 13,
+   "id": "a0f2ee03",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enternum2:16\n"
+     ]
+    }
+   ],
+   "source": [
+    "D=input(\"enternum2:\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 14,
+   "id": "f8e117ba",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      " the number16 is greater than number6\n"
+     ]
+    }
+   ],
+   "source": [
+    "if A>D:\n",
+    "    print(f\" the number{D} is greater than number{A}\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 24,
+   "id": "2805dab9",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "if A==D:\n",
+    "    print(f\"the number {A}is greater than number{D}\")\n",
+    "else:\n",
+    "    (f\"the number {D} is not greater than number{A}\")"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "89f87a82",
+   "metadata": {},
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 31,
+   "id": "b6f11fcb",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enternum1:7\n",
+      "enternum2:2\n"
+     ]
+    }
+   ],
+   "source": [
+    "A=input(\"enternum1:\")\n",
+    "D=input(\"enternum2:\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 35,
+   "id": "89282d65",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "the number2 is divid by 7\n"
+     ]
+    }
+   ],
+   "source": [
+    "if int(D)%2==0:\n",
+    "    print(f\"the number{D} is divid by 7\")\n",
+    "    "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b046bcad",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.16"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
